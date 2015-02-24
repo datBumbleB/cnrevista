01848dd4f5eeed90abb2a53a05431f53ca35098b
Panainte Adrian <datbumbleb@yahoo.com>
2015-02-24 20:09:46 +0200

    no message

--- JavaScript/divDimensions.js
+++ JavaScript/divDimensions.js
@@ -4,7 +4,7 @@
 var screen_y = $(window).height(); //get screen height
 $("header").css("height", screen_y);
 $("#main-header").css("height", screen_y - 330);
-$("#content-table").css("height", screen_y - 100);
+// $("#content-table").css("height", screen_y - 100);
 console.log(screen_y);
 $(".arrow").offset( {top: (screen_y - 150), left: ($(window).width()/2 - 40)});
 $("#real-header").offset({top: (screen_y/2 - $("#real-header").height()/2), left: 0});
